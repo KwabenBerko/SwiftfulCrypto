4040bbed45dcfde31bb5d0d5030e692bdfb04916
Kwabena Berko <kwabenaberko95@gmail.com>
2023-12-29 23:53:42 +0000

    Initial Commit

---
+++ SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulCryptoApp.swift
+//  SwiftfulCrypto
+//
+//  Created by Kwabena Berko on 12/29/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
