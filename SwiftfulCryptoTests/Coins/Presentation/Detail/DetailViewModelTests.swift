4d537b8832810c46543ced2cf588daab5fd2bcca
Kwabena Berko <kwabenaberko95@gmail.com>
2024-01-24 22:41:39 +0000

    Cleanup

--- SwiftfulCryptoTests/Coins/Presentation/Detail/DetailViewModelTests.swift
+++ SwiftfulCryptoTests/Coins/Presentation/Detail/DetailViewModelTests.swift
@@ -40,47 +40,4 @@ final class DetailViewModelTests: XCTestCase {
         getCoinDetail.setCoinDetail(nil)
         XCTAssertEqual([true, false], subscriber.values)
     }
-    
-//    func test_IsLoading() throws {
-//        let recorder = sut.$isLoading.record()
-//        
-//        XCTAssertTrue(try wait(for: recorder.next()))
-//        
-//        getCoinDetail.setCoinDetail(coinDetail)
-//        XCTAssertFalse(try wait(for: recorder.next()))
-//    }
-//    
-//    func test_IsError() throws {
-//        let recorder = sut.$isError.record()
-//        
-//        XCTAssertFalse(try wait(for: recorder.next()))
-//        
-//        getCoinDetail.setCoinDetail(nil)
-//        XCTAssertTrue(try wait(for: recorder.next()))
-//    }
-//    
-//    func test_ContentState() throws {
-//        let overviewPublisher = sut.$overviewStatistics.record()
-//        let additionalPublisher = sut.$additionalStatistics.record()
-//        let descriptionPublisher = sut.$coinDescription.record()
-//        let websiteURLPublisher = sut.$websiteURL.record()
-//        let redditURLPublisher = sut.$redditURL.record()
-//        let coinPublisher = sut.$coin.record()
-//        
-//        XCTAssertEqual([], try wait(for: overviewPublisher.next()))
-//        XCTAssertEqual([], try wait(for: additionalPublisher.next()))
-//        XCTAssertEqual(nil, try wait(for: descriptionPublisher.next()))
-//        XCTAssertEqual(nil, try wait(for: websiteURLPublisher.next()))
-//        XCTAssertEqual(nil, try wait(for: redditURLPublisher.next()))
-//        XCTAssertEqual(coin, try wait(for: coinPublisher.next()))
-//        
-//        getCoinDetail.setCoinDetail(coinDetail)
-//        
-//        XCTAssertEqual([], try wait(for: overviewPublisher.next()))
-//        XCTAssertEqual([], try wait(for: additionalPublisher.next()))
-//        XCTAssertEqual(coinDetail.info.readableDescription, try wait(for: descriptionPublisher.next()))
-//        XCTAssertEqual(coinDetail.info.links?.homepage, try wait(for: websiteURLPublisher.next()))
-//        XCTAssertEqual(coinDetail.info.links?.subredditURL, try wait(for: redditURLPublisher.next()))
-//        XCTAssertEqual(coin, try wait(for: coinPublisher.next()))
-//    }
 }
